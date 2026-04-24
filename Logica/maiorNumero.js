dc08b9891f8fc5850e10f0031c3631a3e6619b81
Vitttu <vitormsantana967@gmail.com>
2026-04-24 20:29:49 -0300

    Encontra maior numero atualizado

--- Logica/maiorNumero.js
+++ Logica/maiorNumero.js
@@ -1,19 +1,14 @@
 const numeros = [3, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 34, 4, 5, 6, 7, 8];
 
 function verificaMaior(numero) {
-  for (let i = 0; i < numero.length; i++) {
-    let maior = [];
-    if (numero[i] < numero[i + 1]) {
-      maior = numero[i + 1];
-    } else {
+  let maior = numero[0];
+
+  for (let i = 1; i < numero.length; i++) {
+    if (numero[i] > maior) {
       maior = numero[i];
     }
-    console.log(maior);
   }
-
-  // console.log(numero[i] + " é menor que " + numero[i + 1]);
-
-  //     }
-  //   }
+  console.log(maior);
 }
+
 verificaMaior(numeros);
